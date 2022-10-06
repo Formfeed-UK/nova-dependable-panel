656e124d781d1bb344320678422b6ee4354c9cbc
ianrobertsFF <91917328+ianrobertsFF@users.noreply.github.com>
2022-10-06 22:34:04 +0100

    fix: removed index field from field.js

--- resources/js/field.js
+++ resources/js/field.js
@@ -3,7 +3,6 @@ import DetailField from './components/DetailField'
 import FormField from './components/FormField'
 
 Nova.booting((app, store) => {
-  app.component('index-nova-dependable-panel', IndexField)
   app.component('detail-nova-dependable-panel', DetailField)
   app.component('form-nova-dependable-panel', FormField)
 })

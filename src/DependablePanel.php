257b16368a6a09fd0381768f7abff86dfce4c08c
ianrobertsFF <91917328+ianrobertsFF@users.noreply.github.com>
2022-10-06 22:32:53 +0100

    feat: Support for Preview, Detail, and Index requests
    - getFields added to support filtered fields for views

--- src/DependablePanel.php
+++ src/DependablePanel.php
@@ -12,6 +12,7 @@ use Laravel\Nova\Fields\FieldCollection;
 use Laravel\Nova\Fields\FormData;
 use Laravel\Nova\Http\Requests\NovaRequest;
 use Laravel\Nova\Fields\SupportsDependentFields;
+use Laravel\Nova\Http\Controllers\ResourcePreviewController;
 use Laravel\Nova\Panel;
 
 use PhpParser\Node\Expr\BinaryOp\BooleanAnd;
@@ -50,6 +51,11 @@ class DependablePanel extends Field {
         $this->default(null);
         $this->attribute = str_replace(' ', '_', Str::lower($name));
         $this->fields = new FieldCollection($fields);
+
+        $request = app(NovaRequest::class);
+        if (count($this->fields->filterForPreview($request, $this->resource)) > 0) {
+            $this->showOnPreview(true);
+        }
     }
 
     public function __clone() {
@@ -148,8 +154,7 @@ class DependablePanel extends Field {
         return $this;
     }
 
-    public function getValidationAttributeNames(NovaRequest $request)
-    {
+    public function getValidationAttributeNames(NovaRequest $request) {
         return array_merge(
             [$this->validationKey() => $this->name],
             $this->fields->mapWithKeys(function ($field) use ($request) {
@@ -158,7 +163,7 @@ class DependablePanel extends Field {
         );
     }
 
-    public function seperate(bool $bool = true, $panelName = null) {
+    public function separatePanel(bool $bool = true, $panelName = null) {
 
         if ($bool === false) {
             $this->panel = null;
@@ -197,10 +202,49 @@ class DependablePanel extends Field {
         }
     }
 
+    public function getFields(NovaRequest $request) {
+        if ($request->isCreateOrAttachRequest()) {
+            return $this->fields->reject(function ($field) use ($request) {
+                return $field instanceof ListableField ||
+                    ($field instanceof ResourceTool || $field instanceof ResourceToolElement) ||
+                    $field->attribute === 'ComputedField' ||
+                    ($field instanceof ID && $field->attribute === $this->resource->getKeyName()) ||
+                    !$field->isShownOnCreation($request);
+            });
+        } else if ($request->isUpdateOrUpdateAttachedRequest()) {
+            return $this->fields->reject(function ($field) use ($request) {
+                return $field instanceof ListableField ||
+                    ($field instanceof ResourceTool || $field instanceof ResourceToolElement) ||
+                    $field->attribute === 'ComputedField' ||
+                    ($field instanceof ID && $field->attribute === $this->resource->getKeyName()) ||
+                    !$field->isShownOnUpdate($request, $this->resource);
+            });
+        } else if ($request->isResourceIndexRequest()) {
+            return $this->fields
+                ->filterForIndex($request, $this->resource)
+                ->withoutListableFields()
+                ->authorized($request)
+                ->resolveForDisplay($this->resource);
+        } else if ($request->route()->getController() instanceof ResourcePreviewController) {
+            return $this->fields
+                ->filterForPreview($request, $this->resource)
+                ->authorized($request)
+                ->resolveForDisplay($this->resource);
+        } else if ($request->isResourceDetailRequest()) {
+            $test = $this->fields
+                ->filterForDetail($request, $this->resource)
+                ->authorized($request)
+                ->resolveForDisplay($this->resource);
+            return $test;
+        } else {
+            return $this->fields->authorized($request);
+        }
+    }
+
     public function jsonSerialize(): array {
         $request = app(NovaRequest::class);
         return array_merge(parent::jsonSerialize(), [
-            'fields' => $this->fields->jsonSerialize(),
+            'fields' => $this->getFields($request)->jsonSerialize(),
             'singleRequest' => $this->singleRequest,
         ]);
     }

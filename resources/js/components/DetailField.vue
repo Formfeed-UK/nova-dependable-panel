a31bb4346a20a6ab72338dd84fa806018a342ea4
ianrobertsFF <91917328+ianrobertsFF@users.noreply.github.com>
2022-10-06 22:31:21 +0100

    feat: DetailField implemented

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -1,9 +1,31 @@
 <template>
-  <PanelItem :index="index" :field="field" />
+  <div class="divide-y divide-gray-100 dark:divide-gray-700">
+    <component
+      :key="index"
+      v-for="(field, index) in field.fields"
+      :index="index"
+      :is="resolveComponentName(field)"
+      :resource-name="resourceName"
+      :resource-id="resourceId"
+      :resource="resource"
+      :field="field"
+      @actionExecuted="actionExecuted"
+    />
+  </div>
 </template>
 
 <script>
 export default {
-  props: ['index', 'resource', 'resourceName', 'resourceId', 'field'],
-}
+  props: ["index", "resource", "resourceName", "resourceId", "field"],
+  methods: {
+    /**
+     * Resolve the component name.
+     */
+    resolveComponentName(field) {
+      return field.prefixComponent
+        ? "detail-" + field.component
+        : field.component;
+    },
+  },
+};
 </script>
